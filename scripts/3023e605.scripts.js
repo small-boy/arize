4dcece9a8fdaafecb33a3ae3a276782f3e2fb23d
anenth <anenthvishnu@gmail.com>
2014-02-26 17:47:18 +0530

    -n Initial commit

---
+++ scripts/3023e605.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("arizeApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("arizeApp").controller("MainCtrl",["$scope",function(){}]);
